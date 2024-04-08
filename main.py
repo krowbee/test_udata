711a1ab5e50e465c2f7ea66f46a7eba83af28f79
krowbee <skoresplay@gmail.com>
2024-04-09 00:31:14 +0300

    mistake fixed

--- main.py
+++ main.py
@@ -3,29 +3,25 @@ import json
 app = FastAPI()
 
 
-
+with open('data.json', 'r') as json_file:
+        data = json.load(json_file)
+        
 
 
 @app.get('/all_products/')
 def root():
     context= {}
-    with open('data.json', 'r') as json_file:
-        data = json.load(json_file)
-        for key in data:
-            context[key] = data[key]['name']
+    for key in data:
+        context[key] = data[key]['name']
     return context
             
 
 @app.get('/products/{product_name}/')
 def product_info(product_name:str):
-    with open('data.json', 'r') as json_file:
-        data = json.load(json_file)
-        context = data[product_name]
+    context = data[product_name]
     return context
 
 @app.get('/products/{product_name}/{product_field}')
 def product_field_info(product_name:str,product_field:str):
-    with open('data.json', 'r') as json_file:
-        data = json.load(json_file)
-        context = data[product_name][product_field]
+    context = data[product_name][product_field]
     return context
